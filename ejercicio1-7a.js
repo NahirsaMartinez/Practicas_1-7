cb61db09f503fa3d8c57ad8872949d094ce41209
Franco, Rafael, Nahir <emaildeprueba7392169@gmail.com>
2023-04-11 11:30:59 -0300

    correcciones

--- ejercicio1-7a.js
+++ ejercicio1-7a.js
@@ -3,4 +3,4 @@ let pedroManzanas= 8
 let susanaManzanas= 9
 let manzanasCesta = mariaManzana + pedroManzanas + susanaManzanas
 
-console.log ("el total de manzanas en la cesta es:" + manzanasCesta)
+console.log ("el total de manzanas en la cesta es:" + manzanasCesta);
