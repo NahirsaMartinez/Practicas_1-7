24308289330d3a207c2eae8d987267de6ac68cd8
Nahir Martinez <nahirmartinez09@gmail.com>
2023-04-12 22:08:52 -0300

    ejercicio f añadido

--- ejercicio1-7f.js
+++ ejercicio1-7f.js
@@ -1,1 +1,6 @@
-let 
+let velociddadFinal = 60;
+let velocidadInicial = 0;
+let aceleracion = 20;
+let tiempoRecorrido = (velociddadFinal - velocidadInicial) / aceleracion;
+
+console.log("La distancia total es "+ tiempoRecorrido);
